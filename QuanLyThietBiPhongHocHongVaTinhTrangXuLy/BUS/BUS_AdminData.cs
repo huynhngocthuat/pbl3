b4554ed6bbe472784a869e9fc65e964a51230f17
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2021-05-23 18:42:05 +0700

    Fix bug: After filter data, get reportId to respond

--- QuanLyThietBiPhongHocHongVaTinhTrangXuLy/BUS/BUS_AdminData.cs
+++ QuanLyThietBiPhongHocHongVaTinhTrangXuLy/BUS/BUS_AdminData.cs
@@ -167,14 +167,109 @@ namespace BUS
         {
             return DAL_AdminData.Instance.DAL_GetReportStatusByReportId(reportId);
         }
-        public List<int> BUS_GetReportIdList()
+        public List<int> BUS_GetReportIdList(string zoneId, int check, int date)
         {
-            List<int> reportIdList = new List<int>();
-            foreach (REPORT item in DAL_AdminData.Instance.DAL_GetALlReports())
+            // zoneId -> "A",......
+            // check -> 1: All, 2: Chua, 3: Roi  
+            // date -> 0: Bao cach day 15 ngay,.....
+
+            List<ReportShow> list1 = new List<ReportShow>();
+            List<ReportShow> list2 = new List<ReportShow>();
+            List<ReportShow> list3 = new List<ReportShow>();
+            List<int> listReportId = new List<int>();
+            // loc theo cbbZone
+            if (zoneId == "")
             {
-                reportIdList.Add(item.reportId);
+                list1 = BUS_ShowAllReports();
             }
-            return reportIdList;
+            else
+            {
+                listReportId = DAL_AdminData.Instance.GetReportIdListByZoneId(zoneId);
+                foreach (ReportShow item in BUS_ShowAllReports())
+                {
+                    for (int i = 0; i < listReportId.Count(); i++)
+                    {
+                        if (listReportId[i] == item.STT)
+                        {
+                            list1.Add(item);
+                        }
+                    }
+                }
+            }
+            // loc theo checkbox
+            if (check == 3)
+            {
+                foreach (ReportShow item in list1)
+                {
+                    if (item.getResponseType() == 0 || item.getResponseType() == 1)
+                    {
+                        list2.Add(item);
+                    }
+                }
+            }
+            else if (check == 2)
+            {
+                foreach (ReportShow item in list1)
+                {
+                    if (item.getResponseType() == 2 || item.getResponseType() == 3)
+                    {
+                        list2.Add(item);
+                    }
+                }
+            }
+            else
+            {
+                list2 = list1;
+            }
+            // loc theo ngay
+            switch (date)
+            {
+                case 0:
+                    foreach (ReportShow item in list2)
+                    {
+                        if ((DateTime.Now - item.reportedDate).TotalDays < 15)
+                        {
+                            list3.Add(item);
+                        }
+                    }
+                    break;
+                case 1:
+                    foreach (ReportShow item in list2)
+                    {
+                        if ((DateTime.Now - item.reportedDate).TotalDays < 30)
+                        {
+                            list3.Add(item);
+                        }
+                    }
+                    break;
+                case 2:
+                    foreach (ReportShow item in list2)
+                    {
+                        if ((DateTime.Now - item.reportedDate).TotalDays < 60)
+                        {
+                            list3.Add(item);
+                        }
+                    }
+                    break;
+                case 3:
+                    foreach (ReportShow item in list2)
+                    {
+                        if ((DateTime.Now - item.reportedDate).TotalDays < 365)
+                        {
+                            list3.Add(item);
+                        }
+                    }
+                    break;
+                default:
+                    list3 = list2;
+                    break;
+            }
+            List<int> resultList = new List<int>();
+            for (int i = 0; i < list3.Count(); i++)
+            {
+                resultList.Add(list3[i].STT);
+            }
+            return resultList;
         }
     }
 }
